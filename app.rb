143a46760f8310cd719d3fb500ad06445ead0be0
Parker Williamson <parker.williamson@gmail.com>
2020-06-15 22:14:25 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   get "/reversename/:name" do 
-    @reverse = [params:name].reverse
+    @reverse = params[:name].reverse
     "#{@reverse}"
   end
 
