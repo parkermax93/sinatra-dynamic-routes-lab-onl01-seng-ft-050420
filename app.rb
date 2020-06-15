51e032c42ecb845fe4d71250b169a7c6f6dba028
Parker Williamson <parker.williamson@gmail.com>
2020-06-15 22:27:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -27,11 +27,18 @@ class App < Sinatra::Base
     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"
   end
   
-  get '/:operation/:number1/:number2'
+  get '/:operation/:number1/:number2' do 
   @number1 = params[:number1].to_i 
   @number2 = params[:number2].to_i
   @operation = :operation
-  if @operation == "add"
-    "#{@number1 + @number2"
-    
+    if @operation == "add"
+      "#{@number1 + @number2}"
+    elsif @operation == "subtract"
+      "#{@number1 - @number2}"
+    elsif @operation == "multiply"
+      "#{@number1 * @number2}"
+    else @operation == "divide"
+      "#{@number1 / @number2}"
+    end
+  end
 end
