d63032c5ecc7b0b79f51a68b9b61d92086ca45ad
Parker Williamson <parker.williamson@gmail.com>
2020-06-15 22:24:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -31,6 +31,7 @@ class App < Sinatra::Base
   @number1 = params[:number1].to_i 
   @number2 = params[:number2].to_i
   @operation = :operation
-  if 
-
+  if @operation == "add"
+    "#{@number1 + @number2"
+    
 end
