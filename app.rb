3abd303a33b674dc015e9ab1ed3576be1958e498
Parker Williamson <parker.williamson@gmail.com>
2020-06-15 22:15:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,5 +6,10 @@ class App < Sinatra::Base
     @reverse = params[:name].reverse
     "#{@reverse}"
   end
+  
+  get "/square/:number" do 
+    @square = params[:number].to_i * params[:number].to_i
+    "#{@square}"
+  end
 
 end
