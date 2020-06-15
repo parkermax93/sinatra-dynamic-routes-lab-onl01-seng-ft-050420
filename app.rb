ab2594c89887222a993749738b1a8a47c3ebabc4
Parker Williamson <parker.williamson@gmail.com>
2020-06-15 22:29:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do 
   @number1 = params[:number1].to_i 
   @number2 = params[:number2].to_i
-  @operation = :operation
+  @operation = params[:operation]
     if @operation == "add"
       "#{@number1 + @number2}"
     elsif @operation == "subtract"
